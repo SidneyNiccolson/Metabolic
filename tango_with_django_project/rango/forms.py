19aa8d63a4ff2ebd3ab1faa0d8a75e47db5f5cd3
Sidney Niccolson <sidneyniccolson@gmail.com>
2014-10-15 14:10:10 +0200

    tango_with_django_project/

--- tango_with_django_project/rango/forms.py
+++ tango_with_django_project/rango/forms.py
@@ -1,1 +1,57 @@
-__author__ = 'sidney'
+from django import forms
+from django.contrib.auth.models import  User
+from models import Page, Category, UserProfile
+
+class CategoryForm(forms.ModelForm):
+    name = forms.CharField(max_length=128, help_text="Please enter the category name.")
+    views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+    likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+
+    class Meta:
+        # association between the modelForm and a actual model
+        model = Category
+
+class PageForm(forms.ModelForm):
+    title = forms.CharField(max_length=128, help_text="Please enter the title of the page.")
+    url = forms.URLField(max_length=200, help_text="Please enter the URL of the page.")
+    views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+
+    class Meta:
+        # Provide an association between the ModelForm and a model
+        model = Page
+
+        # What fields do we want to include in our form?
+        # This way we don't need every field in the model present.
+        # Some fields may allow NULL values, so we may not want to include them...
+        # Here, we are hiding the foreign key.
+        fields = ('title', 'url', 'views')
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        url = cleaned_data.get('url')
+
+        #if url is not empty and does not start with http
+        if url and not url.startswith('http://'):
+            url = 'http://' + url
+            cleaned_data['url'] = url
+
+        return cleaned_data
+
+#user registration to rango
+class UserForm(forms.ModelForm):
+    username = forms.CharField(help_text="Please enter a username.")
+    email = forms.CharField(help_text="Please enter a email adress.")
+    password = forms.CharField(widget=forms.PasswordInput(), help_text="Please enter a password.")
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+
+class UserProfileForm(forms.ModelForm):
+    website = forms.URLField(help_text="Please enter your website.", required=False)
+    picture = forms.ImageField(help_text="Select a profile image to upload", required=False)
+
+    class Meta:
+        model = UserProfile
+        fields = ('website', 'picture')
+
--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -1,1 +1,22 @@
-__author__ = 'sidney'
+from django.conf.urls import patterns, url
+import views
+
+#the first url() is the rango app itself. It contains an empty string regular expression. This is because rango is run first when entering ../rango
+urlpatterns = patterns('',
+    url(r'^$', views.index, name='index'),
+                     #second url() is the rango/about/
+    url(r'^about/', views.index2, name='index2'),
+    url(r'^add_category/$', views.add_category, name='add_category'), # NEW MAPPING!
+    url(r'^category/(?P<category_name_url>\w+)/add_page/$', views.add_page, name='page'),
+    #third url() is the rango/category/<category name>
+    url(r'^category/(?P<category_name_url>\w+)/$', views.category, name='category'),
+    url(r'^register/$', views.register, name='register'),
+    url(r'^login/$', views.user_login, name='login'),
+    url(r'^restricted/', views.restricted, name='restricted'),
+    url(r'^logout/$', views.user_logout, name='logout'),
+    url(r'^search/$',views.search, name='search'),
+    url(r'^profile/$', views.profile, name='profile'),
+    url(r'^goto/$', views.track_url, name='track_url'),
+    url(r'^like_category/$', views.like_category, name='like_category'),
+
+    )
